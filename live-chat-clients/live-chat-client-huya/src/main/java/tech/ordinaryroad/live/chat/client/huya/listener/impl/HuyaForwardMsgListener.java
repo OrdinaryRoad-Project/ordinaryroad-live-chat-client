a472d2f68c439277e4c5b4af8f8095b73d1851a5
OrdinaryRoad <or-mjz@qq.com>
2024-03-09 10:17:37 +0800

    perf: ForwardMsgListener构造函数增加参数校验

--- live-chat-clients/live-chat-client-huya/src/main/java/tech/ordinaryroad/live/chat/client/huya/listener/impl/HuyaForwardMsgListener.java
+++ live-chat-clients/live-chat-client-huya/src/main/java/tech/ordinaryroad/live/chat/client/huya/listener/impl/HuyaForwardMsgListener.java
@@ -27,6 +27,7 @@ package tech.ordinaryroad.live.chat.client.huya.listener.impl;
 import cn.hutool.core.util.StrUtil;
 import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
+import tech.ordinaryroad.live.chat.client.commons.base.exception.BaseException;
 import tech.ordinaryroad.live.chat.client.commons.base.msg.IMsg;
 import tech.ordinaryroad.live.chat.client.huya.listener.IHuyaMsgListener;
 import tech.ordinaryroad.live.chat.client.huya.msg.MessageNoticeMsg;
@@ -43,17 +44,18 @@ import java.nio.charset.StandardCharsets;
  */
 public class HuyaForwardMsgListener implements IHuyaMsgListener {
 
-    private WebSocketLiveChatClient webSocketLiveChatClient;
+    private final WebSocketLiveChatClient webSocketLiveChatClient;
 
     public HuyaForwardMsgListener(String webSocketUri) {
-        if (StrUtil.isNotBlank(webSocketUri)) {
-            webSocketLiveChatClient = new WebSocketLiveChatClient(
-                    WebSocketLiveChatClientConfig.builder()
-                            .websocketUri(webSocketUri)
-                            .build()
-            );
-            webSocketLiveChatClient.connect();
+        if (StrUtil.isBlank(webSocketUri)) {
+            throw new BaseException("转发地址不能为空");
         }
+        webSocketLiveChatClient = new WebSocketLiveChatClient(
+                WebSocketLiveChatClientConfig.builder()
+                        .websocketUri(webSocketUri)
+                        .build()
+        );
+        webSocketLiveChatClient.connect();
     }
 
     @Override
