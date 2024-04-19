d145b38692874959f171e4f41b581138ee3b43c2
OrdinaryRoad <or-mjz@qq.com>
2024-04-19 16:01:53 +0800

    test

--- live-chat-clients/live-chat-client-huya/src/test/java/tech/ordinaryroad/live/chat/client/huya/client/HuyaLiveChatClientTest.java
+++ live-chat-clients/live-chat-client-huya/src/test/java/tech/ordinaryroad/live/chat/client/huya/client/HuyaLiveChatClientTest.java
@@ -69,8 +69,10 @@ class HuyaLiveChatClientTest {
                 .roomId(333003)
 //                .roomId(575757)
 
-                // 自己的
                 .roomId(29785782)
+
+                .roomId(333003)
+                .roomId("bagea")
                 .build();
 
         client = new HuyaLiveChatClient(config, new IHuyaMsgListener() {
@@ -96,13 +98,13 @@ class HuyaLiveChatClientTest {
 
             @Override
             public void onMsg(HuyaBinaryFrameHandler binaryFrameHandler, IMsg msg) {
-//                log.debug("{} 收到{}消息 {}", binaryFrameHandler.getRoomId(), msg.getClass(), msg);
+                log.debug("{} 收到{}消息 {}", binaryFrameHandler.getRoomId(), msg.getClass(), msg);
             }
 
             @Override
             public void onCmdMsg(HuyaBinaryFrameHandler binaryFrameHandler, HuyaCmdEnum cmd, ICmdMsg<HuyaCmdEnum> cmdMsg) {
                 switch (cmd) {
-                    case BeginLiveNotice:{
+                    case BeginLiveNotice: {
                         // 开始直播
                         //18:16:59.622 [nioEventLoopGroup-2-1] INFO tech.ordinaryroad.live.chat.client.huya.client.HuyaLiveChatClientTest -- 29785782 收到CMD消息BeginLiveNotice {"smsg":"AmQypb8RDtEmBumfs+S5kDxABVkAAQoGAkFMHCJkMqW/MmQypb9CZDKlv1ZAMTY4MTA0MDgzMS0xNjgxMDQwODMxLTcyMjAwMTUzOTIzODU2NjI5NzYtMzM2MjIwNTExOC0xMDA1Ny1BLTAtMWYfaHR0cDovL2FsLmZsdi5odXlhLmNvbS9odXlhbGl2ZXYDZmx2hnF3c1NlY3JldD1mMGU4MzY0MjNjNDA5YzEzMzM4N2FhYzA1OGFhNzY3YiZ3c1RpbWU9NjVlZDg4OWImZm09UkZkeE9FSmpTak5vTmtSS2REWlVXVjhrTUY4a01WOGtNbDhrTXclM0QlM0QmZnM9YmdjdJYfaHR0cDovL2FsLmhscy5odXlhLmNvbS9odXlhbGl2ZaYEbTN1OLZxd3NTZWNyZXQ9ZjBlODM2NDIzYzQwOWMxMzMzODdhYWMwNThhYTc2N2Imd3NUaW1lPTY1ZWQ4ODliJmZtPVJGZHhPRUpqU2pOb05rUktkRFpVV1Y4a01GOGtNVjhrTWw4a013JTNEJTNEJmZzPWJnY3TAA9AB4GTwD2TwEGT5EQz8EvYTH2h0dHA6Ly9hbC5wMnAuaHV5YS5jb20vaHV5YWxpdmX2FAVzbGljZfYVcXdzU2VjcmV0PWYwZTgzNjQyM2M0MDljMTMzMzg3YWFjMDU4YWE3NjdiJndzVGltZT02NWVkODg5YiZmbT1SRmR4T0VKalNqTm9Oa1JLZERaVVdWOGtNRjhrTVY4a01sOGtNdyUzRCUzRCZmcz1iZ2N08BYC/Bf5GAz4GQz8GvwbC2kAAQYCQUxzZe2IluaXt0OMnKy8xhM/U3RydWdnbGXjgpvmi7zmkI8/0nIb+gbs/A/wEAH5EQABCgYG6LaF5riFHCw8QP9QAWx2AIYAlgCmALzMC/YSDOmaj+S+v+WQrOWQrPwT9hRgaHR0cDovL2h1eWFpbWcubXNzdGF0aWMuY29tL2F2YXRhci8xMDE4LzFlL2ZjYzRhMjNkZjlmOTc0NmZiNTEwNTcxOTY3NjBjY18xODBfMTM1LmpwZz8xNDgyNjYzMzI58BUG9hYA9hcA8hhl7YiX8hlkMqW/8hpkMqW/9hsA8BwB8B0I+B4ADgYKQXBwVmVyc2lvbhYHMTIuMC4yMgYIQmFzZWJhbmQWElY4MTYuMC4yNC4yLjI2LkRFVgYKRGV2aWNlTmFtZRYJTTIxMDJKMlNDBgpIVVlBX01BSVhVFgEyBhJIdXlhQXVkaW9BQ1FFbmFibGUWATEGCExpdmVNb2RlFgprU2NyZWVuQ0FQBgdOZXR3b3JrFgR3aWZpBgpTREtWZXJzaW9uFgAGDVN5c3RlbVZlcnNpb24WAjE0BghUaW1lWm9uZRYNQXNpYS9TaGFuZ2hhaQYQZW5hYmxlX211bHRpbGluaxYBMAYJZW5hYmxlX3BrFgEwBg9rZXlfY2FzdGVyX2xpdmUWATEGB3JlYWxfdWEWHGFkcl9saXZlX2tpd2kmNS40MC4zMCZ4aWFvbWn8H/IgAcZ+tvwh8CIB/CP8JPwl","lmsgId":1374002143939868673,"cmd":"8000","cmdEnum":"BeginLiveNotice","luri":8000}
                         //18:16:59.622 [nioEventLoopGroup-2-1] DEBUG tech.ordinaryroad.live.chat.client.huya.client.HuyaLiveChatClientTest -- 29785782 收到其他CMD消息 BeginLiveNotice
@@ -113,7 +115,7 @@ class HuyaLiveChatClientTest {
                         //18:16:59.624 [nioEventLoopGroup-2-1] INFO tech.ordinaryroad.live.chat.client.huya.client.HuyaLiveChatClientTest -- 29785782 收到CMD消息BeginLiveNotice {"smsg":"AmQypb8RDtEmBumfs+S5kDxABVkAAgoGAkFMHCJkMqW/MmQypb9CZDKlv1ZAMTY4MTA0MDgzMS0xNjgxMDQwODMxLTcyMjAwMTUzOTIzODU2NjI5NzYtMzM2MjIwNTExOC0xMDA1Ny1BLTAtMWYfaHR0cDovL2FsLmZsdi5odXlhLmNvbS9odXlhbGl2ZXYDZmx2hnF3c1NlY3JldD1mMGU4MzY0MjNjNDA5YzEzMzM4N2FhYzA1OGFhNzY3YiZ3c1RpbWU9NjVlZDg4OWImZm09UkZkeE9FSmpTak5vTmtSS2REWlVXVjhrTUY4a01WOGtNbDhrTXclM0QlM0QmZnM9YmdjdJYfaHR0cDovL2FsLmhscy5odXlhLmNvbS9odXlhbGl2ZaYEbTN1OLZxd3NTZWNyZXQ9ZjBlODM2NDIzYzQwOWMxMzMzODdhYWMwNThhYTc2N2Imd3NUaW1lPTY1ZWQ4ODliJmZtPVJGZHhPRUpqU2pOb05rUktkRFpVV1Y4a01GOGtNVjhrTWw4a013JTNEJTNEJmZzPWJnY3TAA9AB4GTwD2TwEGT5EQz8EvYTH2h0dHA6Ly9hbC5wMnAuaHV5YS5jb20vaHV5YWxpdmX2FAVzbGljZfYVcXdzU2VjcmV0PWYwZTgzNjQyM2M0MDljMTMzMzg3YWFjMDU4YWE3NjdiJndzVGltZT02NWVkODg5YiZmbT1SRmR4T0VKalNqTm9Oa1JLZERaVVdWOGtNRjhrTVY4a01sOGtNdyUzRCUzRCZmcz1iZ2N08BYC/Bf5GAz4GQz8GvwbCwoGAkhTHCJkMqW/MmQypb9CZDKlv1ZAMTY4MTA0MDgzMS0xNjgxMDQwODMxLTcyMjAwMTUzOTIzODU2NjI5NzYtMzM2MjIwNTExOC0xMDA1Ny1BLTAtMWYaaHR0cDovL2hzLmZsdi5odXlhLmNvbS9zcmN2A2ZsdoZxd3NTZWNyZXQ9ZjBlODM2NDIzYzQwOWMxMzMzODdhYWMwNThhYTc2N2Imd3NUaW1lPTY1ZWQ4ODliJmZtPVJGZHhPRUpqU2pOb05rUktkRFpVV1Y4a01GOGtNVjhrTWw4a013JTNEJTNEJmZzPWJnY3SWGmh0dHA6Ly9ocy5obHMuaHV5YS5jb20vc3JjpgRtM3U4tnF3c1NlY3JldD1mMGU4MzY0MjNjNDA5YzEzMzM4N2FhYzA1OGFhNzY3YiZ3c1RpbWU9NjVlZDg4OWImZm09UkZkeE9FSmpTak5vTmtSS2REWlVXVjhrTUY4a01WOGtNbDhrTXclM0QlM0QmZnM9YmdjdMAO0AHs/A/8EPkRDPwS9hMfaHR0cDovL2hzLnAycC5odXlhLmNvbS9odXlhbGl2ZfYUBXNsaWNl9hVxd3NTZWNyZXQ9ZjBlODM2NDIzYzQwOWMxMzMzODdhYWMwNThhYTc2N2Imd3NUaW1lPTY1ZWQ4ODliJmZtPVJGZHhPRUpqU2pOb05rUktkRFpVV1Y4a01GOGtNVjhrTWw4a013JTNEJTNEJmZzPWJnY3T8FvwX+RgM+BkM/Br8GwtpAAQGAkFMBgJUWAYCSFcGAkhTc2XtiJbml7dDjJysvMYTP1N0cnVnZ2xl44Kb5ou85pCPP9JyG/oG7PwP8BAB+REAAQoGBui2hea4hRwsPED/UAFsdgCGAJYApgC8zAv2Egzpmo/kvr/lkKzlkKz8E/YUYGh0dHA6Ly9odXlhaW1nLm1zc3RhdGljLmNvbS9hdmF0YXIvMTAxOC8xZS9mY2M0YTIzZGY5Zjk3NDZmYjUxMDU3MTk2NzYwY2NfMTgwXzEzNS5qcGc/MTQ4MjY2MzMyOfAVBvYWAPYXAPIYZe2Il/IZZDKlv/IaZDKlv/YbAPAcAfAdCPgeAA4GCkFwcFZlcnNpb24WBzEyLjAuMjIGCEJhc2ViYW5kFhJWODE2LjAuMjQuMi4yNi5ERVYGCkRldmljZU5hbWUWCU0yMTAySjJTQwYKSFVZQV9NQUlYVRYBMgYSSHV5YUF1ZGlvQUNRRW5hYmxlFgExBghMaXZlTW9kZRYKa1NjcmVlbkNBUAYHTmV0d29yaxYEd2lmaQYKU0RLVmVyc2lvbhYABg1TeXN0ZW1WZXJzaW9uFgIxNAYIVGltZVpvbmUWDUFzaWEvU2hhbmdoYWkGEGVuYWJsZV9tdWx0aWxpbmsWATAGCWVuYWJsZV9waxYBMAYPa2V5X2Nhc3Rlcl9saXZlFgExBgdyZWFsX3VhFhxhZHJfbGl2ZV9raXdpJjUuNDAuMzAmeGlhb21p/B/yIAHGfrb8IfAiAfwj/CT8JQ==","lmsgId":1374002144233469953,"cmd":"8000","cmdEnum":"BeginLiveNotice","luri":8000}
                         break;
                     }
-                    case CheckRoomStatus:{
+                    case CheckRoomStatus: {
                         // 点击关闭直播
                         //18:17:33.446 [nioEventLoopGroup-2-1] INFO tech.ordinaryroad.live.chat.client.huya.client.HuyaLiveChatClientTest -- 29785782 收到CMD消息CheckRoomStatus {"smsg":"CQw=","lmsgId":1374002423406325760,"cmd":"6340","cmdEnum":"CheckRoomStatus","luri":6340}
                         break;
@@ -163,7 +165,12 @@ class HuyaLiveChatClientTest {
         client.addMsgListener(new IHuyaMsgListener() {
             @Override
             public void onMsg(IMsg msg) {
-                log.info("收到消息{}", msg);
+//                log.info("收到消息{}", msg);
+            }
+
+            @Override
+            public void onDanmuMsg(MessageNoticeMsg messageNoticeMsg) {
+                log.info("收到弹幕{}:{}", messageNoticeMsg.getUsername(), messageNoticeMsg.getContent());
             }
         });
 
