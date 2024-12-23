dc118b4cb042fcb1379e8abaed5328d83d045ca1
OrdinaryRoad <1962247851@qq.com>
2024-12-23 20:18:25 +0800

    revert

--- live-chat-clients/live-chat-client-kuaishou/src/main/java/tech/ordinaryroad/live/chat/client/kuaishou/config/KuaishouLiveChatClientConfig.java
+++ live-chat-clients/live-chat-client-kuaishou/src/main/java/tech/ordinaryroad/live/chat/client/kuaishou/config/KuaishouLiveChatClientConfig.java
@@ -53,12 +53,12 @@ public class KuaishouLiveChatClientConfig extends BaseNettyClientConfig {
      * {@link RoomInfoGetTypeEnum#NOT_COOKIE}：
      * 不使用Config中配置的Cookie初始化直播间信息
      * <p>
-     * 默认：{@link RoomInfoGetTypeEnum#NOT_COOKIE}
+     * 默认：{@link RoomInfoGetTypeEnum#COOKIE}
      *
      * @see RoomInfoGetTypeEnum
      */
     @Builder.Default
-    private RoomInfoGetTypeEnum roomInfoGetType = RoomInfoGetTypeEnum.NOT_COOKIE;
+    private RoomInfoGetTypeEnum roomInfoGetType = RoomInfoGetTypeEnum.COOKIE;
 
     public void setRoomInfoGetType(RoomInfoGetTypeEnum roomInfoGetType) {
         RoomInfoGetTypeEnum oldValue = this.roomInfoGetType;
