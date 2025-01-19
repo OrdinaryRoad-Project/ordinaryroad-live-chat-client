3c42605d780762cedfb3189581a3a1c57239c2c5
OrdinaryRoad <1962247851@qq.com>
2025-01-19 15:39:25 +0800

    feat: 解析虎牙房间直播流信息

--- live-chat-client-codec/live-chat-client-codec-huya/src/main/java/tech/ordinaryroad/live/chat/client/codec/huya/api/HuyaApis.java
+++ live-chat-client-codec/live-chat-client-codec-huya/src/main/java/tech/ordinaryroad/live/chat/client/codec/huya/api/HuyaApis.java
@@ -30,6 +30,7 @@ import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import cn.hutool.http.HttpStatus;
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.Cleanup;
 import lombok.extern.slf4j.Slf4j;
 import tech.ordinaryroad.live.chat.client.codec.huya.api.response.TtPlayerCfg;
@@ -66,14 +67,19 @@ public class HuyaApis {
     private static final String PATTERN_TT_PROFILE_INFO = "TT_PROFILE_INFO = (\\{.*\\}?);";
     private static final String PATTERN_TT_PLAYER_CFG = "TT_PLAYER_CFG = (\\{.*?\\});";
     private static final String PATTERN_TT_PLAYER_CONF = "TT_PLAYER_CONF = (\\{.*?\\});";
+    private static final String PATTERN_HY_PLAYER_CONFIG_STREAM = "hyPlayerConfig\\s*=\\s*\\{[\\s\\S]*?stream:\\s*(\\{[\\s\\S]*?\\})\\s*\\}";
 
     public static RoomInitResult roomInit(Object roomId) {
-        return roomInit(roomId, null);
+        return roomInit(roomId);
     }
 
-    public static RoomInitResult roomInit(Object roomId, RoomInitResult roomInitResult) {
+    public static RoomInitResult roomInit(Object roomId, String cookie) {
+        return roomInit(roomId, cookie, null);
+    }
+
+    public static RoomInitResult roomInit(Object roomId, String cookie, RoomInitResult roomInitResult) {
         @Cleanup
-        HttpResponse response = createGetRequest("https://www.huya.com/" + roomId, null).execute();
+        HttpResponse response = createGetRequest("https://www.huya.com/" + roomId, cookie).execute();
         if (response.getStatus() != HttpStatus.HTTP_OK) {
             throw new BaseException("获取" + roomId + "真实房间ID失败");
         }
@@ -86,15 +92,18 @@ public class HuyaApis {
         String ttProfileInfoJsonString = ReUtil.getGroup1(PATTERN_TT_PROFILE_INFO, body);
         String ttPlayerCfgJsonString = ReUtil.getGroup1(PATTERN_TT_PLAYER_CFG, body);
         String ttPlayerConfJsonString = ReUtil.getGroup1(PATTERN_TT_PLAYER_CONF, body);
+        String hyPlayerConfigStreamJsonString = ReUtil.getGroup1(PATTERN_HY_PLAYER_CONFIG_STREAM, body);
         TtRoomData ttRoomData = null;
         TtProfileInfo ttProfileInfo = null;
         TtPlayerCfg ttPlayerCfg = null;
         TtPlayerConf ttPlayerConf = null;
+        JsonNode hyPlayerConfigStream = null;
         try {
             ttRoomData = OrJacksonUtil.getInstance().readValue(ttRoomDataJsonString, TtRoomData.class);
             ttProfileInfo = OrJacksonUtil.getInstance().readValue(ttProfileInfoJsonString, TtProfileInfo.class);
             ttPlayerCfg = OrJacksonUtil.getInstance().readValue(ttPlayerCfgJsonString, TtPlayerCfg.class);
             ttPlayerConf = OrJacksonUtil.getInstance().readValue(ttPlayerConfJsonString, TtPlayerConf.class);
+            hyPlayerConfigStream = OrJacksonUtil.getInstance().readTree(hyPlayerConfigStreamJsonString);
         } catch (Exception e) {
             if (log.isWarnEnabled()) {
                 log.warn("直播间信息解析失败", e);
@@ -109,6 +118,7 @@ public class HuyaApis {
         roomInitResult.setTtProfileInfo(ttProfileInfo);
         roomInitResult.setTtPlayerCfg(ttPlayerCfg);
         roomInitResult.setTtPlayerConf(ttPlayerConf);
+        roomInitResult.setHyPlayerConfigStream(hyPlayerConfigStream);
         return roomInitResult;
     }
 
--- live-chat-client-codec/live-chat-client-codec-huya/src/main/java/tech/ordinaryroad/live/chat/client/codec/huya/room/RoomInitResult.java
+++ live-chat-client-codec/live-chat-client-codec-huya/src/main/java/tech/ordinaryroad/live/chat/client/codec/huya/room/RoomInitResult.java
@@ -24,6 +24,11 @@
 
 package tech.ordinaryroad.live.chat.client.codec.huya.room;
 
+import cn.hutool.core.codec.Base64;
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.crypto.digest.DigestUtil;
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -33,7 +38,19 @@ import tech.ordinaryroad.live.chat.client.codec.huya.api.response.TtPlayerConf;
 import tech.ordinaryroad.live.chat.client.codec.huya.api.response.TtProfileInfo;
 import tech.ordinaryroad.live.chat.client.codec.huya.api.response.TtRoomData;
 import tech.ordinaryroad.live.chat.client.commons.base.constant.RoomLiveStatusEnum;
+import tech.ordinaryroad.live.chat.client.commons.base.constant.RoomLiveStreamQualityEnum;
+import tech.ordinaryroad.live.chat.client.commons.base.exception.BaseException;
 import tech.ordinaryroad.live.chat.client.commons.base.room.IRoomInitResult;
+import tech.ordinaryroad.live.chat.client.commons.base.room.IRoomLiveStreamInfo;
+import tech.ordinaryroad.live.chat.client.commons.base.room.RoomLiveStreamInfo;
+import tech.ordinaryroad.live.chat.client.commons.util.OrLiveChatHttpUtil;
+import tech.ordinaryroad.live.chat.client.commons.util.OrLiveChatUrlUtil;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Data
 @AllArgsConstructor
@@ -49,6 +66,7 @@ public class RoomInitResult implements IRoomInitResult {
     private TtProfileInfo ttProfileInfo;
     private TtPlayerCfg ttPlayerCfg;
     private TtPlayerConf ttPlayerConf;
+    private JsonNode hyPlayerConfigStream;
 
     @Override
     public String getRoomTitle() {
@@ -68,5 +86,70 @@ public class RoomInitResult implements IRoomInitResult {
             default:
         }
         return roomLiveStatus;
+    }
+
+    @Override
+    public List<IRoomLiveStreamInfo> getRoomLiveStreamUrls() {
+        List<IRoomLiveStreamInfo> liveStreamInfos = new ArrayList<>();
+        if (!hyPlayerConfigStream.has("data") || hyPlayerConfigStream.withArray("data").isEmpty()) {
+            return liveStreamInfos;
+        }
+        for (JsonNode data : hyPlayerConfigStream.withArray("data")) {
+            if (!data.has("gameStreamInfoList") || data.withArray("gameStreamInfoList").isEmpty()) {
+                continue;
+            }
+            List<String> urls = new ArrayList<>();
+            for (JsonNode jsonNode : data.withArray("gameStreamInfoList")) {
+                urls.add(buildStreamUrl(jsonNode));
+            }
+            liveStreamInfos.add(RoomLiveStreamInfo.builder()
+                    .quality(RoomLiveStreamQualityEnum.Q_UNKNOWN)
+                    .urls(urls)
+                    .build()
+            );
+        }
+        return liveStreamInfos;
+    }
+
+
+    /**
+     * 构建直播流URL
+     *
+     * @param stream stream信息的JSON对象
+     * @return 完整的直播流URL
+     */
+    private static String buildStreamUrl(JsonNode stream) {
+        String sFlvUrl = stream.get("sFlvUrl").asText();
+        String sStreamName = stream.get("sStreamName").asText();
+        String sFlvUrlSuffix = stream.get("sFlvUrlSuffix").asText();
+        String sFlvAntiCode = stream.get("sFlvAntiCode").asText();
+
+        String queryString;
+        try {
+            Map<String, String> urlQuery = OrLiveChatHttpUtil.decodeParamMap(sFlvAntiCode, StandardCharsets.UTF_8);
+
+            int platformId = 100;
+            long uid = RandomUtil.randomLong(12340000, 12349999);
+            long convertUid = (uid << 8 | uid >>> (32 - 8)) & 0xFFFFFFFFL;
+
+            String wsTime = urlQuery.get("wsTime");
+            long seqId = uid + Instant.now().toEpochMilli();
+
+            String fm = urlQuery.get("fm");
+            String decodedFm = new String(Base64.decode(OrLiveChatUrlUtil.decode(fm)));
+            String wsSecretPrefix = decodedFm.split("_")[0];
+
+            String wsSecretHashInput = StrUtil.format("{}|{}|{}", seqId, urlQuery.get("ctype"), platformId);
+            String wsSecretHash = DigestUtil.md5Hex(wsSecretHashInput);
+
+            String wsSecretInput = StrUtil.format("{}_{}_{}_{}_{}", wsSecretPrefix, convertUid, sStreamName, wsSecretHash, wsTime);
+            String wsSecret = DigestUtil.md5Hex(wsSecretInput);
+
+            queryString = StrUtil.format("wsSecret={}&wsTime={}&seqid={}&ctype={}&ver=1&fs={}&u={}&t={}&sv=202412311405&sdk_sid={}&codec=264&a_block=0",
+                    wsSecret, wsTime, seqId, urlQuery.get("ctype"), urlQuery.get("fs"), convertUid, platformId, Instant.now().toEpochMilli());
+        } catch (Exception e) {
+            throw new BaseException("Failed to generate query string", e);
+        }
+        return StrUtil.format("{}/{}.{}?{}", sFlvUrl, sStreamName, sFlvUrlSuffix, queryString);
     }
 }
