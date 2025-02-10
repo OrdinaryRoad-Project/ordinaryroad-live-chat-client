2e758926888520e22888c04eb607183011b0ff64
OrdinaryRoad <1962247851@qq.com>
2025-02-10 20:37:36 +0800

    refactor: add module `live-chat-client-plugins`, `live-chat-client-plugin-forward`

---
+++ live-chat-client-plugins/live-chat-client-plugin-forward/src/main/java/tech/ordinaryroad/live/chat/client/plugin/forward/Main.java
@@ -0,0 +1,13 @@
+package tech.ordinaryroad.live.chat.client.plugin.forward;
+
+/**
+ * ${TODO}
+ *
+ * @author mjz
+ * @date 2025/2/10
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
